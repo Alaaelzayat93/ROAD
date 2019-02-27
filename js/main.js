05bb005d1dd086dfa6138bd02ae6d3a1b61eda90
eng alaa_elzayat <alaaelzayat2018@gmail.com>
2019-02-27 22:35:15 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  
+
+  $('#camera_wrap').camera();
+});
+/*carousel*/
+     $(".owl-carousel").owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    option:true,
+    dots:true,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
